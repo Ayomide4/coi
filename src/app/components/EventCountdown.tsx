703b242157aa9499c0d895a95e9031b4fd5866fa
Ayomide Omotosho <aomotosho4@gmail.com>
2025-01-26 13:10:51 -0600

    fix image spacing 2xl breakpoint

--- src/app/components/EventCountdown.tsx
+++ src/app/components/EventCountdown.tsx
@@ -6,7 +6,6 @@ import CountdownDisplay from "./Countdown";
 
 export default function EventCountdown() {
   //TODO:  registration link
-  // FIX: images on 1440
   const notify = () => toast("Registration will open soon!");
   return (
     <div className="px-4 bg-background py-4 w-full min-h-[900px] relative flex flex-col items-center justify-center">
@@ -14,8 +13,8 @@ export default function EventCountdown() {
         A NIGHT WHERE <span className="text-primary">WORSHIP</span> MEETS{" "}
         <span className="text-primary">TRUTH.</span>
       </h1>
-      <div className="w-full min-h-[700px] relative flex flex-col items-center mt-6 ">
-        <div className="absolute top-32 w-5/6 h-[400px] md:w-3/4 lg:w-2/3">
+      <div className="w-full min-h-[700px] relative flex flex-col items-center mt-6  2xl:min-h-[1000px] 2xl:mb-40">
+        <div className="absolute top-32 w-5/6 h-[400px] md:w-3/4 lg:w-2/3 2xl:h-full ">
           <Image
             src="/assets/coi-footer-3.jpg"
             fill
@@ -23,7 +22,7 @@ export default function EventCountdown() {
             className="object-cover"
           />
         </div>
-        <div className="absolute top-0 right-0 w-40 h-80 md:w-60 md:right-10 lg:right-12 lg:w-64">
+        <div className="absolute top-0 right-0 w-40 h-80 md:w-60 md:right-10 lg:right-12 lg:w-64 2xl:h-3/4 2xl:w-[500px]">
           <Image
             src="/assets/coi-footer-1.jpg"
             fill
@@ -31,7 +30,7 @@ export default function EventCountdown() {
             className="object-cover"
           />
         </div>
-        <div className="absolute left-0 bottom-0 w-44 h-72 md:left-10 md:bottom-10 md:w-60 lg:w-64 lg:h-80 lg:bottom-8">
+        <div className="absolute left-0 bottom-0 w-44 h-72 md:left-10 md:bottom-10 md:w-60 lg:w-64 lg:h-80 lg:bottom-8 2xl:h-3/4 2xl:w-[500px] 2xl:-bottom-60">
           <Image
             src="/assets/coi-footer-2.jpg"
             fill
